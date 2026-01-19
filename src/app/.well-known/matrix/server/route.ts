5d990b310da7c5b3ee6eeeeea68e41fcce8afe18
Z <animal21xxl@gmail.com>
2026-01-19 09:32:46 +0100

    Update server address to include port 443

--- src/app/.well-known/matrix/server/route.ts
+++ src/app/.well-known/matrix/server/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const json = { "m.server": "matrix.zoe.rocks" };
+  const json = { "m.server": "matrix.zoe.rocks:443" };
   return NextResponse.json(json, {
     headers: { 'Content-Type': 'application/json' },
   });
